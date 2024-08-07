0303f844e9719c54d23db594a52764878529f492
Amitkumarsharma7082 <aksharma7404@gmail.com>
2024-08-08 00:14:40 +0530

    oestructuring object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -56,10 +56,19 @@ const users = [
 
 // console.log(users[1].email); // b@email.com
 
-console.log(Object.keys(tinderUser)); // array : [ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(tinderUser)); // array : [ '123abc', 'sammy', false ]
-console.log(Object.entries(tinderUser)); // all value > ['','']
+// console.log(Object.keys(tinderUser)); // array : [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); // array : [ '123abc', 'sammy', false ]
+// console.log(Object.entries(tinderUser)); // all value > ['','']
 
-//check the value present or not >
-console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true
-console.log(tinderUser.hasOwnProperty("isLogged")); // false
+// //check the value present or not >
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true
+// console.log(tinderUser.hasOwnProperty("isLogged")); // false
+
+//Destructuring
+const course = {
+  coursename: "JS",
+  email: "a@gmail.com",
+  courseInstructor: "Amit",
+};
+const { courseInstructor: instructor } = course;
+console.log(instructor);
